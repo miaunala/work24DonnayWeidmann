030b5a454afa2176f634bf78cffa93d241e150c7
Nathalie Guibert <91286684+miaunala@users.noreply.github.com>
2024-05-23 14:58:03 +0200

    Update Pipeline 23.5.
    Integrated removal of NAs & duplicates after every step. Revisited data and joins. Created a variable for proportions of global posts and calculated a linear regression with fixed effects for country & year.

--- merge_data.R
+++ merge_data.R
@@ -1,55 +1,62 @@
 library(readr)
 library(dplyr)
+library(texreg)
 setwd("C:/Users/whatt/Desktop/2024_ResAss_UZHIPZ_WeidmannDonnay")
 
 
-# this is the original dataset where topic models and translations were run (without all additional columns)
+# This is the original dataset where topic models and translations were run (without all additional columns)
 #org_eo2_sample <- read_csv("data/data_sample_eo2_tw_fb_all.csv.gz")
 
 # this is the original dataset with all additional columns
 #org_eo2_original <- read_csv("data/data_sample_eo2_tw_fb_all_original_data.csv.gz")
 
+# Read in data, create necessary variables, delete NAs and duplicates
+
+# example_data
 example_data <- read_csv("example_data.csv")
-epro_t_organisv<- read_csv("epro_t.csv")
-gesis_eo2_latest_pub <- read_csv("eo2_with_latest_publication_date.csv")
+example_data$year <- as.numeric(substr(example_data$time, 7, 10))
+example_data <- example_data %>% filter_at(vars(year, epr_groupid, country_gwid, accountname), all_vars(!is.na(.)))
+example_data <- example_data %>% filter(!duplicated(select(., index_n)))
+#Alternative version: example_data <- example_data[!duplicated(example_data), ]
+
+# epro_t
+epro_t<- read_csv("epro_t.csv")
+epro_t <- epro_t[!is.na(epro_t$orgname), ]
+epro_t <- epro_t[!duplicated(epro_t), ]
+
+# gesis_eo
+gesis_eo <- read_csv("eo2_with_latest_publication_date.csv")
+gesis_eo <- gesis_eo[!is.na(gesis_eo$orgname), ]
+gesis_eo <- gesis_eo %>% filter(!duplicated(select(., org_id, clean_username, channel_type)))
+#gesis_eo <- gesis_eo[!duplicated(gesis_eo), ]
 
 
 
-# for example_data
-results_example_data <- example_data %>%
-  group_by(epr_groupid, year = substr(time, 7, 10), country_gwid, accountname) %>%
+# Grouping for example_data for posts & global posts
+example_data <- example_data %>%
+  group_by(epr_groupid, year, country_gwid, accountname) %>%
   summarise(
     post_count = n(),
     post_count_global = sum(global ==1),
     .groups = "keep"
   ) %>%
   arrange(epr_groupid, year, country_gwid, accountname)
+example_data <- example_data[!duplicated(example_data), ]
 
-# needed to filter out where epr_groupid is not there as NAs would have been 
-# matched with the post count otherwise during the left join.
-results_example_data <- results_example_data %>%
-  filter(!is.na(epr_groupid))
+# Join example_data & gesis_eo
+joined_data <- example_data %>%
+  left_join(select(gesis_eo, org_id, orgname, fb_clean, gwid), by = c("accountname" = "fb_clean", "country_gwid" = "gwid"))
+joined_data <- joined_data[!duplicated(joined_data), ]
 
-duplicated_rows1 <- results_example_data[duplicated(results_example_data), ]
-print(duplicated_rows1)
+# Join example_data & epro_t
+joined_data <- joined_data %>%
+  left_join(select(epro_t, "Governmental power", "Regional autonomy", "Separatism/irredentism", "org_id", "year", "gwid"), by = c("org_id","year", "country_gwid"="gwid"))
+joined_data <- joined_data[!duplicated(joined_data), ]
+ 
 
+# Proportion of global posts in total posts
+joined_data$prop_global <- joined_data$post_count_global / joined_data$post_count
 
-res_ex_JOIN_eo2_latPub <- results_example_data %>%
-  left_join(select(gesis_eo2_latest_pub, org_id, orgname, fb_clean), by = c("accountname" = "fb_clean"))
-
-
-res_ex_JOIN_eo2_latPub$year <- as.numeric(res_ex_JOIN_eo2_latPub$year)
-
-duplicated_rows2 <- res_ex_JOIN_eo2_latPub[duplicated(res_ex_JOIN_eo2_latPub), ]
-print(duplicated_rows2)
-
-
-final_ex_eo2_epro <- res_ex_JOIN_eo2_latPub %>%
-  left_join(select(epro_t_organisv, "Governmental power", "Regional autonomy", "Separatism/irredentism", "org_id", "year"), by = c("org_id","year"))
-
-duplicated_rows3 <- final_ex_eo2_epro[duplicated(final_ex_eo2_epro), ]
-print(duplicated_rows3)
-
-final_ex_eo2_epro_distinct <- final_ex_eo2_epro %>%
-  distinct()
-
+# Linear regression w/ fixed effects for country and year
+model1 <- lm(prop_global ~ `Regional autonomy` + `Separatism/irredentism` + as.factor(country_gwid) + as.factor(year), data = joined_data)
+summary(model1)
