25a4d854dcc070cbba297ce784a06fdd5e80fa53
Nathalie Guibert <91286684+miaunala@users.noreply.github.com>
2024-06-12 15:43:18 +0200

    Update analysis.R
    used Nils' library for model1 (felm)

--- analysis.R
+++ analysis.R
@@ -1,3 +1,3 @@
-# Linear regression w/ fixed effects for country and year
-model1 <- feols(prop_global ~ `Regional autonomy` + `Separatism/irredentism` | country_gwid + year, data = joined_data)
-model1
+library("lfe")
+model1 <- felm(prop_global ~ `Regional autonomy` + `Separatism/irredentism` | country_gwid + year|0|country_gwid + year, data = joined_data)
+summary(model1)
