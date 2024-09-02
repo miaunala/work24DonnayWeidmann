c6dd258a9495a625d9a80c59f56666232830d205
Nathalie Guibert <91286684+miaunala@users.noreply.github.com>
2024-09-02 14:25:12 +0200

    new version of merge_data with conflict data, different eo2 dataset

--- merge_data.R
+++ merge_data.R
@@ -4,7 +4,7 @@ library(texreg)
 library(fixest)
 
 # for VM
-setwd("/data/nguibe")
+#setwd("/data/nguibe")
 #setwd("C:/Users/whatt/Desktop/2024_ResAss_UZHIPZ_WeidmannDonnay")
 
 
@@ -19,33 +19,32 @@ smdata <- smdata %>% filter(!duplicated(select(., index_n)))
 
 # eprdata
 eprdata<- read_csv("data/epro_t.csv")
+# delete missings
 eprdata <- eprdata[!is.na(eprdata$orgname), ]
+# delete duplicates
 eprdata <- eprdata[!duplicated(eprdata), ]
-# only keep needed variables
-eprdata <- eprdata[, c("Governmental power", "Regional autonomy", "Separatism/irredentism", "org_id", "year", "gwid", "groupid")]
-# rename complicated variables
+# rename variables
 eprdata <- eprdata %>%
   rename(gov_pow = `Governmental power`) %>%
   rename(reg_autonom = `Regional autonomy`) %>%
-  rename(sep_irred = `Separatism/irredentism`)
-
-# eo2data old
-eo2data_old <- read_csv("data/eo2_with_latest_publication_date.csv")
-eo2data_old <- eo2data_old[!is.na(eo2data_old$orgname), ]
-eo2data_old <- eo2data_old %>% filter(!duplicated(select(., org_id, clean_username, channel_type)))
-#eo2data <- eo2data[!duplicated(eo2data), ]
+  rename(sep_irred = `Separatism/irredentism`) %>%
+  rename(eth_claim = `Ethnic claim`)
 # only keep needed variables
-eo2data_old <- eo2data_old[, c("org_id", "orgname", "fb_clean", "gwid")]
+eprdata <- eprdata[, c("gov_pow", "reg_autonom", "sep_irred", "org_id", "year", "gwid", "groupid", "eth_claim")]
 
-
-# eo2data new
+# eo2data
 eo2data <- read_csv("data/data_sample_eo2_tw_fb_all_original_data.csv.gz")
+# delete missings
 eo2data <- eo2data[!is.na(eo2data$orgname),]
-#org_id [org_id_from_coll], clean_username [accountname], channel type exists muss noch, 
-# keep only needed variables
-# org_id_from_coll, orgname, accountname, gwid.x
-eo2data <- eo2data[, c("org_id_from_coll", "orgname", "accountname", "gwid.x")]
+# only keep needed variables
+eo2data <- eo2data[, c("org_id_from_coll", "orgname", "accountname", "gwid.x", "groupid", "year")]
 
+# power access & conflict data
+powacc_confdata <- read_csv("data/powacc_conflict.csv")
+# removing missings
+powacc_confdata <- powacc_confdata[!is.na(powacc_confdata$groupname),]
+# only keep needed variables
+powacc_confdata <- powacc_confdata[, c("warhist", "peaceyears", "status_excl", "onset_ko_flag", "onset_do_flag", "incidence_flag", "year", "gwgroupid", "groupname")]
 
 # Grouping for smdata for posts & global posts
 smdata <- smdata %>%
@@ -57,24 +56,28 @@ smdata <- smdata %>%
   ) %>%
   arrange(epr_groupid, year, country_gwid, accountname)
 
-# Join smdata & eo2data
-#old data
-joined_data_old <- smdata %>%
-  left_join(select(eo2data_old, org_id, orgname, fb_clean, gwid), by = c("accountname" = "fb_clean", "country_gwid" = "gwid"))
-#new data
+
+# Join Social Media data with EO2 data & remove duplicates
 joined_data <- smdata %>%
-  left_join(select(eo2data, org_id_from_coll, orgname, accountname, gwid.x), by = c("accountname" = "accountname", "country_gwid" = "gwid.x"), relationship = "many-to-many")
+  left_join(select(eo2data, org_id_from_coll, orgname, accountname, gwid.x, year, groupid), by = c("accountname" = "accountname", "country_gwid" = "gwid.x", "year", "epr_groupid"="groupid"))
+joined_data <- joined_data[!duplicated(joined_data),]
 
 
-# Join smdata & eprdata
-joined_data_old <- joined_data_old %>%
-  left_join(select(eprdata, "Governmental power", "Regional autonomy", "Separatism/irredentism", "org_id", "year", "gwid", "groupid"), by = c("org_id","year", "country_gwid"="gwid", "epr_groupid"="groupid"))
+# Join previously joined_data with eprdata
 joined_data <- joined_data %>%
-  left_join(select(eprdata, "gov_pow", "reg_autonom", "sep_irred", "org_id", "year", "gwid", "groupid"), by = c("org_id","year", "country_gwid"="gwid", "epr_groupid"="groupid"))
+  left_join(select(eprdata, "gov_pow", "reg_autonom", "sep_irred", "org_id", "year", "gwid", "groupid", "eth_claim"), by = c("org_id_from_coll"="org_id","year", "country_gwid"="gwid", "epr_groupid"="groupid"))
  
+
+# Join previously joined_data with Power Access and Conflict Data
+joined_data <- joined_data %>% left_join(select(powacc_confdata, "warhist", "peaceyears", "status_excl", "onset_ko_flag", "onset_do_flag", "incidence_flag", "year", "gwgroupid", "groupname"), by = c("epr_groupid"="gwgroupid", "year"))
+
 
 # Proportion of global posts in total posts
 joined_data$prop_global <- joined_data$global_posts / joined_data$post_count
+
+# Save joined_data as CSV
+write_csv(x = joined_data, "data/joined_data.csv")
+
 
 
 
